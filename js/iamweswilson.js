bb017141aea3c5842a0aeff34bf08f3c66eb31eb
Wes Wilson <wes@iamweswilson.com>
2014-09-25 15:56:21 -0500

    initial commit

---
+++ js/iamweswilson.js
@@ -0,0 +1,27 @@
+// Bkgd scroll
+$('#who-i-am').scroll(function() {
+    var x = $(this).scrollTop();
+    $(this).css('background-position', '0% ' + parseInt(-x / 10) + 'px');
+});
+
+// Smooth scroll
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+// Fade in icons
+$(".what-graphic").hide();
+
+$(window).bind("scroll", function() {
+    if ($(this).scrollTop() > 520) {
+        $(".what-graphic").fadeIn("slow");
+    } else {
+        $(".what-graphic").stop();
+    }
+});
+
+//Typer
+$('[data-typer-targets]').typer(3000);

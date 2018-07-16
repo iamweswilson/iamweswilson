d55f997fbbeee722a73dd56957ab1e360d56966d
Wes Wilson <wes@iamweswilson.com>
2018-07-16 18:27:31 -0500

    mobile nav

--- js/iamweswilson.js
+++ js/iamweswilson.js
@@ -1,29 +1,84 @@
+// Close mobile nav on click
+$(document).on('click','.navbar-collapse.show',function(e) {
+  if( $(e.target).is('a') ) {
+      $(this).collapse('hide');
+  }
+});
+
 // Shrink logo on scroll
 $(function(){
-  // var vheight = $(window).height()-500;
+
   var $mainLogo = $('.brand');
   var shrinkHeader = $mainLogo.offset().top;
-   $(window).scroll(function() {
-     var scroll = getCurrentScroll();
-       if ( scroll >= shrinkHeader ) {
-            $('.brand-wrapper').addClass('small-brand');
-            $('.more').addClass('hide');
-            $('#sidebar-nav').addClass('show');
-         }
-         else {
-             $('.brand-wrapper').removeClass('small-brand');
-             $('.more').removeClass('hide');
-             $('#sidebar-nav').removeClass('show');
-         }
-   });
+
+  var windowsize = $(window).width();
+
+$(window).on("load resize scroll",function(e){
+  
+  windowsize = $(window).width();
+
+  if (windowsize > 576) {
+
+    if ($('.mobile-nav').hasClass('show')) {
+      $('.mobile-nav').removeClass('show');
+      $('.brand-wrapper').addClass('small-brand');
+      $('.brand-wrapper').removeClass('hide');
+      $('.more').addClass('hide');
+      $('#sidebar-nav').addClass('show');
+      $('.mobile-nav').removeClass('show');
+    }
+
+    $(window).scroll(function() {
+      var scroll = getCurrentScroll();
+
+        if ( scroll >= shrinkHeader ) {
+             $('.brand-wrapper').addClass('small-brand');
+             $('.more').addClass('hide');
+             $('#sidebar-nav').addClass('show');
+             $('.mobile-nav').removeClass('show');
+          }
+          else {
+              $('.brand-wrapper').removeClass('small-brand');
+              $('.more').removeClass('hide');
+              $('#sidebar-nav').removeClass('show');
+          }
+    });
+  }
+  else {
+
+    if ($('.brand-wrapper').hasClass("small-brand")) {
+      $('.brand-wrapper').addClass('hide');
+      $('.more').addClass('hide');
+      $('#sidebar-nav').addClass('hide');
+      $('.mobile-nav').addClass('show');
+    }
+    $(window).scroll(function() {
+      var scroll = getCurrentScroll();
+
+        if ( scroll >= shrinkHeader ) {
+          // $('.brand-wrapper').addClass('hide');
+          $('.more').addClass('hide');
+          $('#sidebar-nav').addClass('hide');
+          $('.mobile-nav').addClass('show');
+        }
+        else {
+            $('.brand-wrapper').removeClass('hide');
+            $('.more').removeClass('hide');
+            $('#sidebar-nav').removeClass('hide');
+            $('.mobile-nav').removeClass('show');
+        }
+    });
+  }
+});
+
  function getCurrentScroll() {
      return window.pageYOffset || document.documentElement.scrollTop;
      }
+
  });
 
 
  // Smooth Scroll
-
  // Select all links with hashes
 $('a[href*="#"]')
 // Remove links that don't actually link to anything
